20a1ba8c7f6a26fbb340f66236c904b9c3280d29
prasunnyD <prasun.sharma730@gmail.com>
2024-02-07 11:19:46 -0500

    creating post api to model

--- nba_projections_website/nbaprojectionsapp/views.py
+++ nba_projections_website/nbaprojectionsapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, HttpResponse
-
+import requests
+import json
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         oefg= request.POST['OEFG']
-        ooreb= request.POST['OFTR']
-        oftr= request.POST['OREB']
+        oftr= request.POST['OFTR']
+        ooreb= request.POST['OREB']
         pace=request.POST["PACE"]
+        minutes = request.POST['minutes']
+        load = {'OEFG': oefg, 'OFTR': oftr, 'OREB': ooreb,'PACE':pace, 'MIN': minutes}
+        json_load = json.dumps(load)
+        response = requests.post("http://127.0.0.1:5000/points-prediction", json=json_load)
+        print(request.POST)
+        print(load)
+        print(response)
     return render(request,"home.html")
