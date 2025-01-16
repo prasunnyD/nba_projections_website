6c8db4955fb0d271145855e963fafabf35d89f4e
prasunnyD <prasun.sharma730@gmail.com>
2025-01-16 16:24:58 -0500

    adding headline stats to PlayerStatistics

--- src/components/PlayerStatistics.jsx
+++ src/components/PlayerStatistics.jsx
@@ -22,9 +22,10 @@ const PlayerStatistics = ({ playerName }) => {
             setLoading(true);
             try {
                 let response = await client.get(`/${playerName}-shooting-splits`);
+                let response2 = await client.get(`/${playerName}-headline-stats`);
     
                 const playerData = response.data[playerName];
-
+                const headlineStats = response2.data[playerName];
                 setData({
                     FG2A: {
                         value: playerData.FG2A,
@@ -55,6 +56,15 @@ const PlayerStatistics = ({ playerName }) => {
                     },
                     EFG_PCT: {
                         value: playerData.EFG_PCT,
+                    },
+                    PTS: {
+                        value: headlineStats.PTS,
+                    },
+                    REB: {
+                        value: headlineStats.REB,
+                    },
+                    AST: {
+                        value: headlineStats.AST,
                     }
                 });
             } catch (error) {
@@ -82,55 +92,67 @@ const PlayerStatistics = ({ playerName }) => {
         )}
         {data && (
             <div className="p-4">
-                
+                <h4 className="text-xl font-bold text-white mb-4">Season Averages</h4>
+                <div className="overflow-x-auto bg-neutral-800 rounded-lg p-4 mb-6">
+                    <table className="min-w-full">
+                        <thead>
+                            <tr className="border-b border-gray-700">
+                                <th className="px-4 py-2 text-md font-large text-gray-300">PTS : {data.PTS.value}</th>
+                                <th className="px-4 py-2 text-md font-large text-gray-300">REB : {data.REB.value}</th>
+                                <th className="px-4 py-2 text-md font-large text-gray-300">AST : {data.AST.value}</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
                 
                 {/* Shooting Splits Table */}
+                <h4 className="text-xl font-bold text-white mb-4">Shooting Splits</h4>
                 <div className="overflow-x-auto bg-neutral-800 rounded-lg p-4 mb-6">
                     <table className="min-w-full">
                         <thead>
                     <tr className="border-b border-gray-700">
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG2M</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG2A</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG2_PCT</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG2 Freq</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG3A</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG3M</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG3_PCT</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG3 Freq</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">FG%</th>
-                        <th className="px-4 py-2 text-left text-sm font-medium text-gray-300">eFG%</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">2PT Made</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">2PT Attempted</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">2PT %</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">2PT Freq</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">3PT Made</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">3PT Attempted</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">3PT %</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">3PT Freq</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">FG %</th>
+                        <th className="px-4 py-2 text-left text-md font-medium text-gray-300">eFG %</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG2M.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG2A.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG2_PCT.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG2A_FREQUENCY.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
-                            {data.FG3A.value}
-                        </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG3M.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
+                            {data.FG3A.value}
+                        </td>
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG3_PCT.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG3A_FREQUENCY.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.FG_PCT.value}
                         </td>
-                        <td className="px-4 py-2 text-white">
+                        <td className="px-4 py-2 text-left text-white">
                             {data.EFG_PCT.value}
                             </td>
                         </tr>
