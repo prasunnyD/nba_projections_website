b51ef4379ecbb5d1dd6e5189e0edf7e2dce76474
Srikar Vedula <srikarv89@gmail.com>
2025-02-06 17:35:39 -0500

    Cleaned up app.jsx. moved the logout feature to maincontainer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,6 @@ function App() {
     setAwayTeam(awayTeam);
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem('token'); // Remove token
-    setAuthenticated(false); // Set authentication to false
-  };
-
   return (
     <div className="min-h-screen bg-neutral-800 flex flex-col">
         {!isAuthenticated ? (
@@ -48,29 +43,8 @@ function App() {
                         teamName={selectedTeam}
                         homeTeam={homeTeam}
                         awayTeam={awayTeam}
+                        setAuthenticated={setAuthenticated} 
                     />
-                </div>
-                <div className="bg-neutral-900 text-white py-4 flex justify-center items-center shadow-lg">
-                    <button
-                        onClick={handleLogout}
-                        className="bg-red-500 hover:bg-red-600 text-white px-6 py-3 rounded-full flex items-center justify-center space-x-2 text-lg font-medium transition duration-300 transform hover:scale-105 shadow-md"
-                    >
-                        <span>Logout</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={2}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15.75 9V5.25a2.25 2.25 0 00-2.25-2.25h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M18.75 12h-12m9 0l-3 3m3-3l-3-3"
-                            />
-                        </svg>
-                    </button>
                 </div>
             </div>
         )}
